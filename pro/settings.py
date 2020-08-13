23c4221f43c1f68bf4dc3a715141b3e62bd193f1
Mohit singh <onetouch9917@gmail.com>
2020-08-13 15:48:23 +0530

    edit

--- pro/settings.py
+++ pro/settings.py
@@ -120,7 +120,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'pro/static')
+]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 #Managing media
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
